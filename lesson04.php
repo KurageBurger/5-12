ba6dda11153f4ddcbbe99a25b6dfe62f611b4878
KurageBurger <ets_purple24746@yahoo.co.jp>
2024-10-25 10:10:17 +0900

    課題あげ直し

---
+++ lesson04.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+class Person {
+    protected $name;
+    protected $age;
+
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function introduce() {
+        return "私の名前は{$this->name}です。{$this->age}歳です。";
+    }
+}
+
+class Teacher extends Person {
+    private $subject;
+
+    public function __construct($name, $age, $subject) {
+        parent::__construct($name, $age);
+        $this->subject = $subject;
+    }
+
+    public function introduce() {
+        return "私は{$this->name}です。{$this->subject}を教えています。";
+    }
+}
---
+++ lesson05.php
@@ -0,0 +1,41 @@
+﻿<?php
+
+interface Media {
+    public function play();
+}
+
+class Music implements Media {
+    private $songTitle;
+
+    public function __construct($songTitle) {
+        $this->songTitle = $songTitle;
+    }
+
+    public function play() {
+        return "[{$this->songTitle}]を再生中";
+    }
+}
+
+class Video implements Media {
+    private $videoTitle;
+
+    public function __construct($videoTitle) {
+        $this->videoTitle = $videoTitle;
+    }
+
+    public function play() {
+        return "[{$this->videoTitle}]を再生中";
+    }
+}
+
+class Podcast implements Media {
+    private $episodeTitle;
+
+    public function __construct($episodeTitle) {
+        $this->episodeTitle = $episodeTitle;
+    }
+
+    public function play() {
+        return "ポッドキャストの[{$this->episodeTitle}]を再生中";
+    }
+}
